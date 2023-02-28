661d2b59501f242b03579aaa60230907bcd45ad2
Francesco Punta <puntafrancesco@gmail.com>
2023-02-28 16:21:00 +0100

    Problemi visualizzazione browser

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!-- javascript -->
+<script>
+// riferimento a AppTitle.vue
+import primoComponenente from './components/AppTitle.vue';
+// riferimento a AppHero.vue
+import secondoComponenente from './components/AppHero.vue';
+//Definizione del componente nella pagina
+export default {
+    components: {
+        // riferimento a AppTitle.vue
+        primoComponenente,
+        // riferimento a AppHero.vue
+        secondoComponenente
+    }
+}
+</script>
+
+
+<!-- html -->
+<template>
+    <!-- inserisco nel template l'elemento title -->
+
+    <!-- riferimento a AppHero.vue -->
+    <primoComponenente />
+
+    <!-- riferimento a AppHero.vue -->
+    <secondoComponenente />
+</template>
+
+
+<!-- CSS -->
+<style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<!-- javascript -->
+<script>
+</script>
+
+
+<!-- html -->
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+
+<!-- CSS -->
+<style>
+h1 {
+    text-align: center;
+    padding: 30px;
+}
+</style>
